a89343c14d89596dda4736f170b6e0a4ebba5fa0
周文童 <zhouwentong@finupgroup.com>
2018-05-23 16:06:38 +0800

    增加了些单元测试

--- collection/src/main/java/com/finup/list/TestArrayList.java
+++ collection/src/main/java/com/finup/list/TestArrayList.java
@@ -2,8 +2,8 @@ package com.finup.list;
 
 import org.junit.Test;
 
-import javax.jnlp.IntegrationService;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -35,7 +35,7 @@ public class TestArrayList {
     }
 
     @Test
-    public void testArrayEquals() throws Exception{
+    public void testArrayEquals() throws Exception {
         Object[] arr1 = {"aa", "bb"};
         Object[] arr2 = {"aa", "bb"};
         System.out.println(arr1 == arr2);
@@ -65,12 +65,12 @@ public class TestArrayList {
     }
 
     @Test
-    public void testToArray() throws Exception{
+    public void testToArray() throws Exception {
 //        TimeUnit.SECONDS.sleep(5);
         List<String> list = new ArrayList<>();
         list.add("a");
         list.add("b");
-        String[] arr = new String[]{"adf",null,null};
+        String[] arr = new String[]{"adf", null, null};
         list.toArray(arr);
         for (String s : arr) {
             System.out.println(s);
@@ -152,6 +152,62 @@ public class TestArrayList {
         list2.add("3");
         list.removeAll(list2);
         System.out.println(list);
+        System.out.println();
+    }
+
+    @Test
+    public void testRemove1() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("e");
+        list.add("h");
+        list.add("c");
+        System.out.println(batchRemove(list, true));
+    }
+
+    private boolean batchRemove(Collection<?> c, boolean complement) {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        list.add("g");
+        list.add("h");
+
+        Object[] objects = list.toArray();
+        int size = objects.length;
+        // 复制一个本地变量，命名有讲究吗？为啥用的是跟之前一样的名字。
+        final Object[] elementData = objects;
+        // 这个应该是有讲究，r -> read,w -> write
+        int r = 0, w = 0;
+        boolean modified = false;
+        for (; r < size; r++) {
+            if (c.contains(elementData[r]) == complement) {
+                // 通过遍历，把当前元素的值放到数组的头部，最后再次清空从 0 到 w 的元素就行
+                System.out.println("w = " + w);
+                elementData[w++] = elementData[r];
+                print(elementData);
+            }
+        }
+
+        if (w != size) {
+            // clear to let GC do its work
+            for (int i = w; i < size; i++) {
+                elementData[i] = null;
+            }
+        }
+        for (Object elementDatum : elementData) {
+            System.out.println(elementDatum + " ");
+        }
+        return modified;
+    }
+
+    private void print(Object[] arr) {
+        for (Object elementDatum : arr) {
+            System.out.print(elementDatum + " ");
+        }
         System.out.println();
     }
 
