871ff6ddc011e46c5797250d821a2e9418022bb6
周文童 <zhouwentong@finupgroup.com>
2018-07-27 16:50:20 +0800

    终于把 gradle 搞好了，原来是 ~/.gradle/gradle.properties 里面的代理导致我的下载地址跑偏了

--- gradle/src/main/java/com/finup/Demo.java
+++ gradle/src/main/java/com/finup/Demo.java
@@ -1,11 +1,14 @@
 package com.finup;
 
+import com.google.common.collect.Lists;
+
 /**
  * Created by zhouwentong on 2018/7/26.
  */
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Lists.newArrayList();
     }
 
 }
