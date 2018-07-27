f3d2553e1240ee34a19ed3ed3789d40e48c8e50e
周文童 <zhouwentong@finupgroup.com>
2018-07-27 14:25:24 +0800

    update

---
+++ gradle/src/main/java/com/finup/Demo.java
@@ -0,0 +1,11 @@
+package com.finup;
+
+/**
+ * Created by zhouwentong on 2018/7/26.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
