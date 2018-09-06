24ff653350c7e206312f8cf9691b53395687f044
周文童 <zhouwentong@finupgroup.com>
2018-09-06 14:05:55 +0800

    增加快速排序算法

---
+++ algorithm/src/main/java/com/finup/demo/DivideAndConquer.java
@@ -0,0 +1,11 @@
+package com.finup.demo;
+
+/**
+ * 分治法
+ */
+public class DivideAndConquer {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ algorithm/src/main/java/com/finup/demo/QuickSort.java
@@ -0,0 +1,44 @@
+package com.finup.demo;
+
+/**
+ * 对数组快速排序
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 2, 10, 4, 6, -1, 0};
+        printArray(arr);
+        quickSort(arr, 0, arr.length);
+        printArray(arr);
+    }
+
+    private static void quickSort(int[] arr,int from,int end) {
+        if (from < end) {
+            int mid = (end - from) / 2;
+            quickSort(arr, from, mid);
+            quickSort(arr, mid + 1, end);
+        }
+    }
+
+    private static void printArray(int[] arr) {
+        for (int o : arr) {
+            System.out.print(o + " ");
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        while (low < high) {
+            while (low < high && arr[high] >= pivot) {
+                high--;
+            }
+            arr[low] = arr[high];
+            while (low < high && arr[low] <= pivot) {
+                low++;
+            }
+            arr[high] = arr[low];
+        }
+        arr[low] = pivot;
+        return low;
+    }
+
+}
