06b950cc8327375e7e40ee75b62b6beb50ce8090
周文童 <zhouwentong@finupgroup.com>
2018-09-10 17:31:55 +0800

    temp

---
+++ algorithm/src/main/java/com/finup/demo/MergeSort.java
@@ -0,0 +1,15 @@
+package com.finup.demo;
+
+/**
+ * 归并排序
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        System.out.println(10 >> 1);
+        int[] arr = {1, 2, 3, 4};
+        int i = 1;
+        System.out.println(arr[i++]);
+    }
+
+
+}
