5b652e346f808cdb3df5b8cdff962ce0a034df97
周文童 <zhouwentong@finupgroup.com>
2019-02-27 14:32:00 +0800

    PriorityQueue 源码

---
+++ sourcecode/src/test/java/com/wentong/demo/TestPriorityQueue.java
@@ -0,0 +1,26 @@
+package com.wentong.demo;
+
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
+/**
+ * 对优先级队列的测试，为 DelayQueue 做准备
+ */
+public class TestPriorityQueue {
+
+    @Test
+    public void testBasicUse() throws Exception{
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        queue.add("b");
+        queue.add("e");
+        queue.add("d");
+        queue.add("d");
+        queue.add("a");
+        queue.add("f");
+        queue.add("c");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(queue.poll());
+        }
+    }
+}
