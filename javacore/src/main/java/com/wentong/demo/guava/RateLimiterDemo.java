e1e38a3e99cf0a61b934f4d946edc79c4144f45a
周文童 <zhouwentong@finupgroup.com>
2019-07-17 19:23:37 +0800

    RateLimiterDemo

---
+++ javacore/src/main/java/com/wentong/demo/guava/RateLimiterDemo.java
@@ -0,0 +1,18 @@
+package com.wentong.demo.guava;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * 使用 Guava 提供的 RateLimiter 简单的限流
+ */
+public class RateLimiterDemo {
+
+    public static void main(String[] args) {
+        RateLimiter rateLimiter = RateLimiter.create(1);
+        for (int i = 1; i < 10; i = i + 2) {
+            double acquireTime = rateLimiter.acquire(i);
+            System.out.println("i = " + i + ",acquireTime = " + acquireTime);
+        }
+    }
+
+}
