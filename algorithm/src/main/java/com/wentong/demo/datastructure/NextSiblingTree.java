f28d8238c3645eefe0af95d706396e9488658aa4
周文童 <819878569@qq.com>
2019-07-22 21:35:51 +0800

    儿子兄弟标识法

---
+++ algorithm/src/main/java/com/wentong/demo/datastructure/NextSiblingTree.java
@@ -0,0 +1,10 @@
+package com.wentong.demo.datastructure;
+
+public class NextSiblingTree {
+
+    class TreeNode {
+        Object element;
+        TreeNode firstChild;
+        TreeNode nextSibling;
+    }
+}
