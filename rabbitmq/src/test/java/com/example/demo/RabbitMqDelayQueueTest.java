155bfb2402f51a29f0833682bac78cf38094a09e
周文童 <zhouwentong@finupgroup.com>
2019-08-05 17:29:55 +0800

    延时队列测试，安装不上插件，未生效

--- rabbitmq/src/test/java/com/example/demo/RabbitMqDelayQueueTest.java
+++ rabbitmq/src/test/java/com/example/demo/RabbitMqDelayQueueTest.java
@@ -11,37 +11,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RabbitMqDelayQueueTest {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
+        try {
 
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
 
-        Map<String, Object> param = new HashMap<>();
-        param.put("x-delayed-type", "direct");
-        channel.exchangeDeclare("x-exchange", "x-delayed-message", true,
-                false, param);
-        Map<String, Object> headers = new HashMap<>();
-        //设置在2016/11/04,16:45:12向消费端推送本条消息
-        Date now = new Date();
-        Date timeToPublish = new Date("2019/08/05,11:02:00");
+            Map<String, Object> param = new HashMap<>();
+            param.put("x-delayed-type", "direct");
+            channel.exchangeDeclare("delay_exchange123", "x-delayed-message", true,
+                    false, param);
+            Map<String, Object> headers = new HashMap<>();
+            Date now = new Date();
+            Date timeToPublish = new Date("2019/08/05,16:37:00");
 
-        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
-        String readyToPushContent = "publish at " + sf.format(now)
-                + " \t deliver at " + sf.format(timeToPublish);
+            String readyToPushContent = "publish at " + sf.format(now)
+                    + " \t deliver at " + sf.format(timeToPublish);
 
-        headers.put("x-delay", timeToPublish.getTime() - now.getTime());
+            headers.put("x-delay", timeToPublish.getTime() - now.getTime());
 
-        AMQP.BasicProperties.Builder props = new AMQP.BasicProperties.Builder()
-                .headers(headers);
-        channel.basicPublish("x-exchange", "test", props.build(),
-                readyToPushContent.getBytes());
+            AMQP.BasicProperties.Builder props = new AMQP.BasicProperties.Builder()
+                    .headers(headers);
+            channel.basicPublish("x-exchange", "test", props.build(),
+                    readyToPushContent.getBytes());
 
-        // 关闭频道和连接
-        channel.close();
-        connection.close();
+            // 关闭频道和连接
+            channel.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
