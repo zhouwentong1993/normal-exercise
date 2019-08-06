c7eb147c328188cdf2c26439eadfdf8f6746232b
周文童 <zhouwentong@finupgroup.com>
2019-08-06 15:28:14 +0800

    logback

--- slf4j-demo/src/main/java/com/wentong/demo/Slf4jDemo1.java
+++ slf4j-demo/src/main/java/com/wentong/demo/Slf4jDemo1.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class Slf4jDemo1 {
     
     public static void main(String[] args) {
-        Logger LOGGER = LoggerFactory.getLogger(Slf4jDemo1.class);
+        Logger LOGGER = LoggerFactory.getLogger(Object.class);
         LOGGER.info("hello world");
         LOGGER.warn("hello world waring");
     }
