aaad9e1e31970e4e026f7522bd777f4e2a8c057d
周文童 <zhouwentong@finupgroup.com>
2019-08-15 14:25:35 +0800

    事务测试

---
+++ spring/src/main/java/com/wentong/TestController.java
@@ -0,0 +1,9 @@
+package com.wentong;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+}
--- spring/src/main/java/com/wentong/TestTransactional.java
+++ spring/src/main/java/com/wentong/TestTransactional.java
@@ -1,5 +1,33 @@
 package com.wentong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
 public class TestTransactional {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestTransactional.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Transactional
+    public void book(String... persons) {
+        for (String person : persons) {
+            LOGGER.info("Booking " + person + " in a seat...");
+            jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person);
+        }
+    }
+
+    public List<String> findAllBookings() {
+        return jdbcTemplate.query("select FIRST_NAME from BOOKINGS",
+                (rs, rowNum) -> rs.getString("FIRST_NAME"));
+    }
 
 }
