27ed62cef09abcd55109ea523af291450b72a67f
周文童 <zhouwentong@finupgroup.com>
2019-09-16 11:26:37 +0800

    基本实现，待压力测试

--- springboot-redisson/src/main/java/com/wentong/springbootredisson/aspect/LockAspect.java
+++ springboot-redisson/src/main/java/com/wentong/springbootredisson/aspect/LockAspect.java
@@ -4,7 +4,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.expression.EvaluationContext;
@@ -14,9 +17,8 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Aspect
@@ -28,17 +30,19 @@ public class LockAspect {
 
     private LocalVariableTableParameterNameDiscoverer discoverer = new LocalVariableTableParameterNameDiscoverer();
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LockAspect.class);
 
     @Around(value = "@annotation(com.wentong.springbootredisson.aspect.DistributeLock)")
     public Object lock(ProceedingJoinPoint jp) throws Throwable {
-        Object object = null;
         MethodSignature signature = (MethodSignature) jp.getSignature();
         Method method = signature.getMethod();
         DistributeLock distributeLock = method.getAnnotation(DistributeLock.class);
 
         String uniqueKey = distributeLock.lockUniqueKey();
+        int expireTime = distributeLock.expireTime();
+        long timeOut = distributeLock.timeOut();
+
         Object[] args = jp.getArgs();
-        Parameter[] parameters = method.getParameters();
         String[] params = discoverer.getParameterNames(method);
         EvaluationContext context = new StandardEvaluationContext();
         for (int len = 0; len < params.length; len++) {
@@ -46,8 +50,19 @@ public class LockAspect {
         }
         Expression expression = spelExpressionParser.parseExpression(uniqueKey);
         Object value = expression.getValue(context);
-
-        Annotation[][] annotations = method.getParameterAnnotations();
-        return object;
+        String lockKey = uniqueKey + value;
+        RLock lock = redissonClient.getLock(lockKey);
+        boolean locked = lock.tryLock(timeOut, expireTime, TimeUnit.SECONDS);
+        if (locked) {
+            try {
+                LOGGER.info("Thread:{} get lock,key is:{}", Thread.currentThread().getName(), lockKey);
+                return jp.proceed();
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            LOGGER.info("Thread:{} can't get lock,key is:{}", Thread.currentThread().getName(), lockKey);
+            throw new IllegalStateException();
+        }
     }
 }
--- springboot-redisson/src/main/java/com/wentong/springbootredisson/controller/TestController.java
+++ springboot-redisson/src/main/java/com/wentong/springbootredisson/controller/TestController.java
@@ -2,7 +2,6 @@ package com.wentong.springbootredisson.controller;
 
 import com.wentong.springbootredisson.aspect.DistributeLock;
 import com.wentong.springbootredisson.vo.User;
-import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,20 +25,9 @@ public class TestController {
     @PostMapping("lock/{name}")
     @DistributeLock(lockUniqueKey = "#user.userId",lockPrefix = "testLock")
     public String getLock(@PathVariable String name, @RequestBody User user) throws Exception {
-        RLock lock = redissonClient.getLock(name);
-        boolean b = lock.tryLock(5,TimeUnit.SECONDS);
-        if (b) {
-            LOGGER.info("get lock by:{}", Thread.currentThread().getName());
-            try {
-                TimeUnit.SECONDS.sleep(5);
-                lock.unlock();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return "getLock";
-        } else {
-            return "unGetLock";
-        }
+        LOGGER.info("getLock");
+        TimeUnit.SECONDS.sleep(1);
+        return "getLock";
     }
 
 }
