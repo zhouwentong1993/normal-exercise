ce33915f1db616066cb2ed9a3ff31c159af7116f
周文童 <zhouwentong@finupgroup.com>
2019-09-24 18:09:09 +0800

    out & error 不同

---
+++ javacore/src/main/java/com/wentong/demo/core/TestPrint.java
@@ -0,0 +1,21 @@
+package com.wentong.demo.core;
+
+public class TestPrint {
+
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(++i);
+        System.out.println(++i);
+        System.out.println(++i);
+        System.out.println(++i);
+        System.out.println(++i);
+        System.out.println(++i);
+        System.err.println(++i);
+        System.err.println(++i);
+        System.err.println(++i);
+        System.err.println(++i);
+        System.err.println(++i);
+        System.err.println(++i);
+    }
+
+}
