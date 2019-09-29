78f9ad099d81c3e31b7af28ddf31e74bd7bd4a20
周文童 <zhouwentong@finupgroup.com>
2019-09-29 16:13:57 +0800

    update

---
+++ druid-demo/src/main/java/com/wentong/demo/DruidApplication.java
@@ -0,0 +1,11 @@
+package com.wentong.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DruidApplication.class, args);
+    }
+}
