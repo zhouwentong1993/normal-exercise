86a261977043d44ac731cfcb4b1b12ec95e7dc9f
周文童 <zhouwentong@finupgroup.com>
2019-11-01 15:31:46 +0800

    Netty 重新学习

---
+++ netty/src/main/java/io/netty/study/DiscardServerHandler.java
@@ -0,0 +1,19 @@
+package io.netty.study;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
