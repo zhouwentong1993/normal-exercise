88d956b0ec92f2006dd98d4df6231eafcacd6f77
周文童 <zhouwentong@finupgroup.com>
2019-11-14 19:12:07 +0800

    update

--- redisson/src/main/java/com/wentong/demo/RedissonLua.java
+++ redisson/src/main/java/com/wentong/demo/RedissonLua.java
@@ -1,6 +1,5 @@
 package com.wentong.demo;
 
-import com.google.common.collect.Lists;
 import org.redisson.Redisson;
 import org.redisson.api.RScript;
 import org.redisson.api.RedissonClient;
@@ -9,6 +8,11 @@ import org.redisson.config.TransportMode;
 
 import java.util.concurrent.Executors;
 
+
+// 要实现监控，Redisson 无法执行 lua 的 info。
+// 可以通过 jedisCluster 来获取集群底下所有的节点，然后分别对各个节点运行 info 命令。
+// 顺便说一句，jedisCluster 不支持 info 命令，直接抛出异常。作者解释到这种命令在集群环境下没有意义，这也是写代码一种非常好的思路
+// 就是不做没有意义的事情。
 public class RedissonLua {
 
     public static void main(String[] args) {
@@ -21,15 +25,23 @@ public class RedissonLua {
 
         RedissonClient redisson = Redisson.create(config);
         String script = "return redis.call('info')";
-        Object eval1 = redisson.getScript().eval(
-                RScript.Mode.READ_ONLY, //执行模式
-                "return redis.call('get', KEYS[1])",    // 要执行的lua脚本
-                RScript.ReturnType.INTEGER, // 返回值类型
-                Lists.newArrayList("tac"),  // 传入KEYS
-                true, 1L, "hello"   //传入ARGV
-        );
-        System.out.println(eval1);
-        Object eval = redisson.getScript().eval(RScript.Mode.READ_WRITE, script, RScript.ReturnType.STATUS);
+//        Object eval1 = redisson.getScript().eval(
+//                RScript.Mode.READ_ONLY, //执行模式
+//                "return redis.call('get', KEYS[1])",    // 要执行的lua脚本
+//                RScript.ReturnType.INTEGER, // 返回值类型
+//                Lists.newArrayList("tac"),  // 传入KEYS
+//                true, 1L, "hello"   //传入ARGV
+//        );
+//        System.out.println(eval1);
+//        BOOLEAN(RedisCommands.EVAL_BOOLEAN),
+//                INTEGER(RedisCommands.EVAL_LONG),
+//                MULTI(RedisCommands.EVAL_LIST),
+//                STATUS(RedisCommands.EVAL_STRING),
+//                VALUE(RedisCommands.EVAL_OBJECT),
+//                MAPVALUE(RedisCommands.EVAL_MAP_VALUE),
+//                MAPVALUELIST(RedisCommands.EVAL_MAP_VALUE_LIST);
+
+        Object eval = redisson.getScript().eval(RScript.Mode.READ_ONLY, script, RScript.ReturnType.STATUS);
         System.out.println(eval);
 
     }
