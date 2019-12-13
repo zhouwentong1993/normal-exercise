6e0c9423a45726a0b27fa824010cc1cc069ebcab
周文童 <zhouwentong@finupgroup.com>
2019-12-13 16:27:29 +0800

    锁相关

--- interview/src/main/java/com/wentong/concurrent/LockThings.java
+++ interview/src/main/java/com/wentong/concurrent/LockThings.java
@@ -1,8 +1,12 @@
 package com.wentong.concurrent;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class LockThings {
+
+    private static AtomicInteger serviceNum = new AtomicInteger(0);
+    private static AtomicInteger ticketNum = new AtomicInteger(0);
 
     private static AtomicReference<Thread> owner = new AtomicReference<>();
 
@@ -22,6 +26,23 @@ public class LockThings {
     // 自旋解锁
     public static void spinUnLock() {
         owner.compareAndSet(Thread.currentThread(), null);
+    }
+
+
+    // 带票据的加锁
+    public static void ticketSpinLock() {
+        // 每次获取一个票据，所有线程都可以。
+        int ticketNow = ticketNum.getAndIncrement();
+        // 但是只有一个线程能跳出 while 循环
+        while (serviceNum.get() != ticketNow) {
+
+        }
+    }
+
+    // 带票据的解锁
+    public static void ticketSpinUnLock(int ticket) {
+        // 这里要将 ticket 置为下一个，这样下一个 ticket 就能获取到锁了。
+        serviceNum.compareAndSet(ticket, ticket + 1);
     }
 
 
