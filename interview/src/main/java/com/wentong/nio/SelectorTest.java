c608bae4ec879a3eebc7c866616bcb648d8689b2
周文童 <zhouwentong@finupgroup.com>
2019-12-24 11:50:23 +0800

    Selector

--- interview/src/main/java/com/wentong/nio/SelectorTest.java
+++ interview/src/main/java/com/wentong/nio/SelectorTest.java
@@ -1,14 +1,30 @@
 package com.wentong.nio;
 
+import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 
 public class SelectorTest {
     public static void main(String[] args) throws Exception {
-        Selector selector = Selector.open();
-        ServerSocketChannel socketChannel = ServerSocketChannel.open();
-        socketChannel.configureBlocking(false);
-        socketChannel.register(selector, SelectionKey.OP_READ);
+        try (Selector selector = Selector.open();
+             ServerSocketChannel socketChannel = ServerSocketChannel.open()) {
+            socketChannel.bind(new InetSocketAddress(9090));
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            int count = selector.select();
+            if (count > 0) {
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+                    if (selectionKey.isAcceptable()) {
+                        SocketChannel accept = socketChannel.accept();
+                        accept.register(selector, SelectionKey.OP_READ);
+                    }
+                }
+            }
+        }
     }
 }
