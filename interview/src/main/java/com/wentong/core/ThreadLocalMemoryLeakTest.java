45641209cbab80ed4bedc639087b628745cd1d13
周文童 <zhouwentong@finupgroup.com>
2020-01-03 15:00:46 +0800

    ThreadLocal

--- interview/src/main/java/com/wentong/core/ThreadLocalMemoryLeakTest.java
+++ interview/src/main/java/com/wentong/core/ThreadLocalMemoryLeakTest.java
@@ -1,27 +1,20 @@
 package com.wentong.core;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * 测试 ThreadLocal 内存泄露
  */
 public class ThreadLocalMemoryLeakTest {
 
-
     public static void main(String[] args) throws Exception {
-        TimeUnit.SECONDS.sleep(15);
         ThreadLocal<BigObject> threadLocal = new ThreadLocal<>();
         BigObject value = new BigObject();
         threadLocal.set(value);
-        value = null;
+        threadLocal = null;
         System.gc();
         System.out.println("ok");
-        TimeUnit.SECONDS.sleep(100);
     }
 }
 
 class BigObject {
     static byte[] bytes = new byte[20480];
-    public BigObject() {
-    }
 }
