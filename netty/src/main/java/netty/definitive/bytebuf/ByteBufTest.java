12806b1c64963c19bb0f0018ece2bc3546c322db
周文童 <zhouwentong@finupgroup.com>
2020-01-14 16:13:08 +0800

    ByteBuf 测试

--- netty/src/main/java/netty/definitive/bytebuf/ByteBufTest.java
+++ netty/src/main/java/netty/definitive/bytebuf/ByteBufTest.java
@@ -1,10 +1,16 @@
 package netty.definitive.bytebuf;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
 
+/*
+    在 ByteBuffer 中，切换读写模式需要调用 flip，但是在 ByteBuf 中，不需要，只需要调用 read write skip 等方法就行
+ */
 public class ByteBufTest {
+
     @Test
     public void testByteBuffer() {
         ByteBuffer byteBuffer = ByteBuffer.allocate(80);
@@ -17,6 +23,18 @@ public class ByteBufTest {
         System.out.println(byteBuffer);
         String s = new String(bytes);
         System.out.println(s);
+    }
 
+    @Test
+    public void testByteBuf() {
+        ByteBuf buffer = Unpooled.buffer(10);
+        System.out.println(buffer);
+        String s = "你好，周文童";
+        buffer.writeBytes(s.getBytes());
+        System.out.println(buffer);
+        byte[] bytes = new byte[s.getBytes().length];
+        buffer.readBytes(bytes);
+        System.out.println(new String(bytes));
+        System.out.println(buffer);
     }
 }
