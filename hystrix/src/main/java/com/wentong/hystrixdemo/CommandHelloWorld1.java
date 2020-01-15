3db6ddd786570e7ba1f79e3b3401c79394958d67
周文童 <zhouwentong@finupgroup.com>
2020-01-15 18:14:28 +0800

    心态不好

--- hystrix/src/main/java/com/wentong/hystrixdemo/CommandHelloWorld1.java
+++ hystrix/src/main/java/com/wentong/hystrixdemo/CommandHelloWorld1.java
@@ -3,6 +3,7 @@ package com.wentong.hystrixdemo;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import rx.Observable;
+import rx.Observer;
 
 import java.util.concurrent.TimeUnit;
 
@@ -23,6 +24,22 @@ public class CommandHelloWorld1 extends HystrixCommand<String> {
     public static void main(String[] args) throws Exception {
         CommandHelloWorld1 helloWorld1 = new CommandHelloWorld1("周文童");
         Observable<String> observe = helloWorld1.observe();
+        observe.subscribe(new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("CommandHelloWorld1.onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println("onNext: " + s);
+            }
+        });
         observe.subscribe(s -> {
             System.out.println("CommandHelloWorld1.call");
             System.out.println(s);
