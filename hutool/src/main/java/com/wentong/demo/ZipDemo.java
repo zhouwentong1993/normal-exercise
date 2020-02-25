97eecf9d7ceabcd7892d23ad22a0e7d1b2f9dac5
周文童 <zhouwentong@finupgroup.com>
2020-02-25 14:21:13 +0800

    update

--- hutool/src/main/java/com/wentong/demo/ZipDemo.java
+++ hutool/src/main/java/com/wentong/demo/ZipDemo.java
@@ -4,20 +4,17 @@ import cn.hutool.core.util.ZipUtil;
 import cn.hutool.crypto.digest.DigestUtil;
 
 import java.io.File;
-import java.io.FileInputStream;
 
 public class ZipDemo {
 
     public static void main(String[] args) throws Exception {
         for (int i = 0; i < 10; i++) {
 
-            File file = new File("/Users/zhouwentong/Desktop/压缩/" + i + ".zip");
+            File file = new File("/Users/finup123/Desktop/压缩/" + i + ".zip");
             long start = System.currentTimeMillis();
-            File zip = ZipUtil.zip(file, true, new File("/Users/zhouwentong/Desktop/test"));
+            File zip = ZipUtil.zip(file, true, new File("/Users/finup123/Desktop/test"));
             String s = DigestUtil.sha256Hex(zip);
-            String s1 = DigestUtil.sha256Hex(new FileInputStream(zip));
             System.out.println(s);
-            System.out.println(s1);
             System.out.println((System.currentTimeMillis() - start));
         }
 
