d18130d309bdcf42727dae158e015f412c13c06f
周文童 <zhouwentong@finupgroup.com>
2020-04-09 18:07:54 +0800

    测试对象大小1

--- javacore/src/main/java/com/wentong/demo/core/ObjectSize.java
+++ javacore/src/main/java/com/wentong/demo/core/ObjectSize.java
@@ -7,18 +7,24 @@ import org.openjdk.jol.vm.VM;
  * 获取对象大小
  */
 public class ObjectSize {
-//    public static void main(String[] args) {
-//        String s = RamUsageEstimator.humanSizeOf(new Object());
-//        System.out.println(s);
-//
-//    }
 
     public static void main(String[] args) throws Exception {
         System.out.println(VM.current().details());
         System.out.println(ClassLayout.parseClass(A.class).toPrintable());
     }
 
+
     public static class A {
-        boolean f;
+        int a;
+        long c;
     }
+
+    public static class B extends A {
+        int b;
+    }
+
+    public static class C extends B {
+        int c;
+    }
+
 }
