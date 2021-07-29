c771644ff1cc239a50571135d386decf05a76d80
zhouwentong <zhouwentong1993@gmail.com>
2021-07-30 00:06:03 +0800

    update

--- caffeine/src/main/java/com/wentong/caffeine/demo/AddCache.java
+++ caffeine/src/main/java/com/wentong/caffeine/demo/AddCache.java
@@ -6,24 +6,49 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.concurrent.TimeUnit;
 
 public class AddCache {
-    public static void main(String[] args) {
-        Cache<String, String> cache = Caffeine.newBuilder()
-                .expireAfterWrite(10, TimeUnit.MINUTES)
+    public static void main(String[] args) throws Exception {
+        Cache<String, Person> cache = Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.SECONDS)
                 .maximumSize(10_000)
                 .build();
 
         // 查找一个缓存元素， 没有查找到的时候返回null
-        String key = "abc";
-        String graph = cache.getIfPresent(key);
+        String name = "Jack";
+        Person someone = cache.getIfPresent(name);
         // 查找缓存，如果缓存不存在则生成缓存元素,  如果无法生成则返回null
-        graph = cache.get(key, k -> createExpensiveGraph(key));
+        someone = cache.get(name, k -> createExpensiveGraph(name));
         // 添加或者更新一个缓存元素
-        cache.put(key, graph);
+        cache.put(name, someone);
+        System.out.println(cache.getIfPresent(name));
+        TimeUnit.SECONDS.sleep(11);
+        System.out.println(cache.getIfPresent(name));
         // 移除一个缓存元素
-        cache.invalidate(key);
+        cache.invalidate(name);
     }
 
-    private static String createExpensiveGraph(String key) {
-        return key;
+    private static Person createExpensiveGraph(String name) {
+        return new Person(name, 10, "北京");
     }
+
+    static class Person {
+        String name;
+        int age;
+        String address;
+
+        public Person(String name, int age, String address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", address='" + address + '\'' +
+                    '}';
+        }
+    }
+
 }
