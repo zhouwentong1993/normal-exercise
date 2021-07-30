3022dfee8dd4fad9b13980b625b4163cf89708d9
zhouwentong <zhouwentong1993@gmail.com>
2021-07-30 23:23:57 +0800

    some demo

--- caffeine/src/main/java/com/wentong/caffeine/demo/AddCache.java
+++ caffeine/src/main/java/com/wentong/caffeine/demo/AddCache.java
@@ -2,11 +2,37 @@ package com.wentong.caffeine.demo;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class AddCache {
     public static void main(String[] args) throws Exception {
+//        loadCacheManual();
+        loadCacheAutomatic();
+    }
+
+    /**
+     * 自动将数据加载进内存中，同步操作
+     */
+    private static void loadCacheAutomatic() {
+        LoadingCache<String, Person> cache = Caffeine.newBuilder()
+                .maximumSize(10_000)
+                .expireAfterWrite(10, TimeUnit.SECONDS)
+                .build(AddCache::createExpensiveGraph);
+        Person p = cache.get("李四");
+        System.out.println(p);
+
+        Map<String, Person> map = cache.getAll(List.of("张三"));
+        System.out.println(map);
+    }
+
+    /**
+     * 手动加载缓存，同步操作
+     */
+    private static void loadCacheManual() throws InterruptedException {
         Cache<String, Person> cache = Caffeine.newBuilder()
                 .expireAfterWrite(10, TimeUnit.SECONDS)
                 .maximumSize(10_000)
