9feb514d84ccf49351d529582c3eb49906018128
zhouwentong <zhouwentong1993@gmail.com>
2021-08-09 22:45:18 +0800

    time-based evict strategy and some refresh strategy,but not working

--- caffeine/src/main/java/com/wentong/caffeine/demo/EvictCache.java
+++ caffeine/src/main/java/com/wentong/caffeine/demo/EvictCache.java
@@ -1,11 +1,14 @@
 package com.wentong.caffeine.demo;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Expiry;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.checkerframework.checker.index.qual.NonNegative;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 测试驱逐缓存
@@ -38,5 +41,46 @@ public class EvictCache {
         }
         ArrayList<String> list = new ArrayList<>(cache.asMap().values());
         System.out.println(list);
+    }
+
+    @Test
+    public void testExpireTimeStrategy() {
+        // 基于固定时间的超时
+        LoadingCache<String, String> cache = Caffeine.newBuilder().expireAfterAccess(10, TimeUnit.MINUTES).build(key -> key);
+        // 基于更多的配置，可以按照创建时间，更新时间和读取时间来计算逻辑。
+        LoadingCache<String, String> cache1 = Caffeine.newBuilder().expireAfter(new Expiry<String, String>() {
+            @Override
+            public long expireAfterCreate(String key, String value, long currentTime) {
+                return 0;
+            }
+
+            @Override
+            public long expireAfterUpdate(String key, String value, long currentTime, @NonNegative long currentDuration) {
+                return 0;
+            }
+
+            @Override
+            public long expireAfterRead(String key, String value, long currentTime, @NonNegative long currentDuration) {
+                return 0;
+            }
+        }).build(key -> key);
+    }
+
+    @Test
+    public void testExpireReferenceStrategy() {
+        // 当key和缓存元素都不再存在其他强引用的时候驱逐
+        LoadingCache<String, String> cache = Caffeine.newBuilder()
+                .weakKeys()
+                .weakValues()
+                .build(key -> key);
+
+        // 当进行GC的时候进行驱逐
+        LoadingCache<String, String> graphs = Caffeine.newBuilder()
+                .softValues()
+                .build(key -> key);
+        graphs.get("a");
+        System.out.println(graphs.get("a"));
+        System.gc();
+        System.out.println(graphs.get("a"));
     }
 }
