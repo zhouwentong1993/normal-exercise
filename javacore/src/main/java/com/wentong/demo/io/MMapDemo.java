a425041cef9e67de707b20af986df85af9822f57
zhouwentong <zhouwentong1993@gmail.com>
2021-11-12 16:54:56 +0800

    sample

--- javacore/src/main/java/com/wentong/demo/io/MMapDemo.java
+++ javacore/src/main/java/com/wentong/demo/io/MMapDemo.java
@@ -3,16 +3,17 @@ package com.wentong.demo.io;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.concurrent.TimeUnit;
 
 public class MMapDemo {
 
     private static final int TEN_MB = 1024 * 1024 * 10;
 
     public static void main(String[] args) throws Exception {
-        try (RandomAccessFile raf = new RandomAccessFile("/Users/finup123/1.pdf", "rw")) {
+        try (RandomAccessFile raf = new RandomAccessFile("/Users/zhouwentong/Desktop/1.txt", "rw")) {
             MappedByteBuffer mappedByteBuffer = raf.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, TEN_MB);
-            byte[] bytes = new byte[4096];
-            mappedByteBuffer.put(bytes);
+            mappedByteBuffer.put("Hello world".getBytes());
+            TimeUnit.SECONDS.sleep(10);
         }
     }
 
