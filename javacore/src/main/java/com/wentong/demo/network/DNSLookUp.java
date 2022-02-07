585b4c2dfbe2ef5ab390f9b323eae918040fe6a2
zhouwentong <zhouwentong1993@gmail.com>
2022-02-07 17:38:16 +0800

    DNS 寻址

---
+++ javacore/src/main/java/com/wentong/demo/network/DNSLookUp.java
@@ -0,0 +1,14 @@
+package com.wentong.demo.network;
+
+import java.net.InetAddress;
+
+public class DNSLookUp {
+
+    public static void main(String[] args) throws Exception {
+        InetAddress[] allByName = InetAddress.getAllByName("www.baidu.com");
+        System.out.println(allByName.length);
+        System.out.println(allByName[0].getHostAddress());
+        System.out.println(allByName[1].getHostAddress());
+    }
+
+}
