892446f11f4969f59653afe734cd2896930d7e43
zhouwentong <zhouwentong@hualala.com>
2022-06-16 11:41:26 +0800

    excel

---
+++ spring-amqp/src/main/java/com/wentong/Main.java
@@ -0,0 +1,19 @@
+package com.wentong;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+public class Main {
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = new CachingConnectionFactory();
+        AmqpAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.declareQueue(new Queue("myqueue"));
+        AmqpTemplate template = new RabbitTemplate(connectionFactory);
+        template.convertAndSend("myqueue", "foo");
+        String foo = (String) template.receiveAndConvert("myqueue");
+    }
+}
